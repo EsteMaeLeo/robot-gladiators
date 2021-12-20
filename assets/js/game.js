20583f555dc9766b8d205104dec172407e8e1cd0
EsteMaeLeo <leonel_morales20@hotmail.com>
2021-12-20 17:47:54 -0600

    javascript first lines

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! Javascript is running!");
+//window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+window.alert(playerName);
+function fight() {
+    window.alert("The fight has begun!");
+
+}
+
+fight();
